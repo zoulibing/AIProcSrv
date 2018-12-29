b4f08d6c916d42fcaf73f0a38fe0d1bbdc96f1bd
alex.han <alex.han@goertek.com>
2018-12-29 17:15:02 +0800

    by zoulibing

--- include/mainframe/monitor.h
+++ include/mainframe/monitor.h
@@ -5,15 +5,20 @@
 #include <string>
 #include <boost/algorithm/string.hpp>
 #include <threads/threadpool.hpp>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
 using namespace std;
 using namespace boost;
+using namespace cv;
 
 
 namespace rock {
     class Monitor
     {
     public:
-        Monitor(string &name,string &uri,string &desc);
+        Monitor(string &name,string &cam_uri,string &desc,string publish_uri);
         bool init();
         bool start(boost::threadpool::pool *_tpp);
         bool stop();
@@ -21,15 +26,20 @@ namespace rock {
 
     private:
         string _name;
-        string _uri;
+        string _camera_uri;
+        string _publish_uri;
         string _desc;
-        void on_thread(Monitor * monitor);
-        void proc();
+        void static on_thread(Monitor * monitor);
+        bool proc();
+        void publish();
 
         int no;
         int max_waiting_ai_result_time;//
         int enable;
+        int open_cam_retry_times;
         boost::threadpool::pool *_tpp;
+        cv::VideoCapture vcap;
+
 
 
 
--- src/mainframe/monitor.cpp
+++ src/mainframe/monitor.cpp
@@ -1,10 +1,37 @@
 #include <mainframe/monitor.h>
 using namespace rock;
 
-Monitor::Monitor(string &name,string &uri,string &desc)
+Monitor::Monitor(string &name,string &cam_uri,string &desc,string publish_uri)
+    :max_waiting_ai_result_time(100),no(-1),enable(false),_tpp(0x00),open_cam_retry_times(3)
 {
 
 }
+
+bool Monitor::init()
+{
+
+}
+
+bool Monitor::start(threadpool::pool *_tpp)
+{
+
+    int idx=0;
+    while(!this->vcap.isOpened() && idx<open_cam_retry_times)
+    {
+        this->vcap.open(this->_camera_uri);
+        boost::this_thread::sleep(boost::posix_time::seconds(1));
+        idx+=1;
+
+    }
+    if(!this->vcap.isOpened())
+        return false;
+
+
+    //begin thread of tpp
+    _tpp->schedule(boost::bind(on_thread, this));
+
+}
+
 
 void Monitor::on_thread(Monitor * monitor)
 {
@@ -14,13 +41,29 @@ void Monitor::on_thread(Monitor * monitor)
     while(mo->enable)
     {
 
-        mo->proc();
+        if(mo->proc()==false)
+            break;
         boost::this_thread::sleep(boost::posix_time::milliseconds(10));
     }
+     mo->enable=false;
 
 }
 
-void Monitor::proc()
+bool Monitor::proc()
 {
+
+     if(!this->vcap.isOpened())
+       return false;
+     //this->vcap.read()
+
+
+    //1.get a frame from cap
+
+    //2.check tasks status,if all tasks finished in time
+
+    //3.if outoftime and other+
+
+    //4.
+
 
 }
