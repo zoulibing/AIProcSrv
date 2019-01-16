b15d11a69da3662c2c4d59fe9a03ba510652eb1f
alex.han <alex.han@goertek.com>
2019-01-16 17:32:24 +0800

    by zoulibing

---
+++ include/mainframe/aiservice.h
@@ -0,0 +1,45 @@
+#ifndef AISERVICE_H
+#define AISERVICE_H
+#include <iostream>
+#include <thrift/transport/TSocket.h>
+#include <thrift/transport/TBufferTransports.h>
+#include <thrift/protocol/TBinaryProtocol.h>
+#include <thirft-gen/AIProReq.h>
+using namespace apache::thrift;
+using namespace apache::thrift::protocol;
+using namespace apache::thrift::transport;
+
+
+using namespace std;
+
+namespace rock {
+    class AIService
+    {
+        public:
+            AIService(int id,string name,string srv_ip,int srv_port,int stand_w,int stand_h,string desc,bool visible);
+            bool start();
+            bool stop();
+            bool release();
+            bool addTask();
+
+        private:
+            int stand_width;
+            int stand_height;
+            float scalar_w;
+            float scalar_h;
+            string name;
+            string ip;
+            int id;
+            int port;
+            int visible;
+            string desc;
+            TSocket * socket;
+            TBufferedTransport* transport;
+            TBinaryProtocol *protocol;
+            AIProReqClient *client;
+
+    };
+
+}
+
+#endif // AISERVICE_H
---
+++ src/mainframe/aiservice.cpp
@@ -0,0 +1,56 @@
+#include <mainframe/aiservice.h>
+using namespace rock;
+
+AIService::AIService(int id,string name,string srv_ip,int srv_port,int stand_w,int stand_h,string desc,bool visible)
+{
+    this->id=id;
+    this->name=name;
+    this->ip=srv_ip;
+    this->port=srv_port;
+    this->stand_height=stand_h;
+    this->stand_width=stand_w;
+    this->desc=desc;
+    this->visible=visible;
+
+}
+
+bool AIService::addTask()
+{
+
+}
+
+bool AIService::start()
+{
+      socket=new TSocket(ip,port);
+      transport=new TBufferedTransport(boost::shared_ptr<TSocket> (socket));
+      protocol=new TBinaryProtocol(boost::shared_ptr<TTransport>(transport));
+      client=new AIProReqClient(boost::shared_ptr<TProtocol>(protocol));
+      int time=0;
+      while(!transport->isOpen() && time <60)
+      {
+               transport->open();
+               sleep(1);
+               time ++;
+      }
+      if(!transport->isOpen())
+          return false;
+
+    return true;
+}
+bool AIService::stop()
+{
+    transport->close();
+    if(transport->isOpen())
+        return false;
+    return true;
+}
+bool AIService::release()
+{
+   if(stop())
+       return false;
+   delete socket;
+   delete transport;
+   delete protocol;
+   delete client;
+    return true;
+}
